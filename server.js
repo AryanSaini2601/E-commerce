58349cc20ce7a21a2de7df59f6ce47041706ad7d
Aryan Saini <aryansaini@Aryans-MacBook-Pro.local>
2023-09-03 19:41:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
+import cors from 'cors'
+
+//rest object
+const app = express();
+
+
+//configure .env
+dotenv.config();
+
+//database config
+connectDB();
+
+
+//middleware
+//for fine working of cross-platform 
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routes
+app.use('/api/v1/auth', authRoutes)
+
+//rest api
+  
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run or listen
+app.listen(PORT, ()=>{
+    console.log(`Server running on ${PORT}`.bgRed.white);
+})
